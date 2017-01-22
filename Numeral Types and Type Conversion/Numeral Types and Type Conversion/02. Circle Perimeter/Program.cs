5680fc4432d93f4e4d29ff54abda4dad8c04b5dd
Hristiyan <dogman19991@gmail.com>
2017-01-22 13:20:36 +0200

    Lab

---
+++ Numeral Types and Type Conversion/Numeral Types and Type Conversion/02. Circle Perimeter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Circle_Perimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double peremeter = 2 * Math.PI * r;
+            Console.WriteLine($"{peremeter:f12}");
+        }
+    }
+}
